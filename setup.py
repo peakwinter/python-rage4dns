5669a56b02bb1d4d677f5542d6b0b87454a8f4bd
Jacob Cook <jacob@peakwinter.net>
2015-05-27 12:06:10 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='python-rage4dns',
+    version='0.1',
+    description='Python bindings for RAGE4 DNS API',
+    author='Jacob Cook',
+    author_email='jacob@coderouge.co',
+    url='https://git.coderouge.co/coderouge/python-rage4dns',
+    py_modules=['rage4'],
+    download_url = 'https://git.coderouge.co/coderouge/python-rage4dns/repository/archive.tar.gz?ref=0.1',
+    license = 'GPLv3'
+)
